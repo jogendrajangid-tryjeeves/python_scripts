2d9684a4848913242436b8bdce443bdc9b22e2e2
Jogendra Kumar <jogendra.kumar@raweng.com>
2018-03-13 16:08:20 +0530

    Added volume verification script

---
+++ list_of_volume_without_proper_tag.py
@@ -0,0 +1,26 @@
+import boto3
+import pprint
+
+region='us-east-1'
+
+Check_tag1='Product'
+Check_tag2='Name'
+Check_tag3='Environment'
+
+ec2=boto3.client('ec2', region_name=region)
+
+#Check volume without proper tag
+Volume_without_tags = { "data": []}
+Volume_data = ec2.describe_volumes()
+for vol_list in Volume_data['Volumes']:
+	tag_keys = {'tags':[]}
+	if 'Tags' in vol_list:
+		for prams in vol_list['Tags']:
+			tag_keys['tags'].append(prams['Key'])
+	 		if ( Check_tag1 and Check_tag2 and Check_tag3 ) not in tag_keys['tags']:
+	 			Volume_without_tags['data'].append(vol_list['VolumeId'])
+	 	else:
+	 		Volume_without_tags['data'].append(vol_list['VolumeId'])
+
+print(len(Volume_without_tags['data']),"Volumes without Proper tags")
+pprint.pprint(Volume_without_tags['data'])
