e1fa5666eed05be45372fe032bfa611bf57e2d5f
Jogendra Kumar <jogendra.kumar@raweng.com>
2018-03-13 15:21:30 +0530

    "Added check for instance without any tags"

--- list_of_Instance_without_proper_tag.py
+++ list_of_Instance_without_proper_tag.py
@@ -15,9 +15,12 @@ Instances_data = ec2.describe_instances()
 for inst_list in Instances_data['Reservations']:
 	tag_keys = {'tags':[]}
 	for value in inst_list['Instances']:
-		for prams in value['Tags']:
-			tag_keys['tags'].append(prams['Key'])
-	 	if ( Check_tag1 and Check_tag2 and Check_tag3 ) not in tag_keys['tags']:
+		if 'Tags' in value:
+			for prams in value['Tags']:
+				tag_keys['tags'].append(prams['Key'])
+	 		if ( Check_tag1 and Check_tag2 and Check_tag3 ) not in tag_keys['tags']:
+	 			Instance_without_tags['data'].append(value['InstanceId'])
+	 	else:
 	 		Instance_without_tags['data'].append(value['InstanceId'])
 
 print(len(Instance_without_tags['data']),"Instances without Proper tags")
